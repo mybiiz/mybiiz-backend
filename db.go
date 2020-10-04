ed29fb8a0bd4b6aaa3bc87e91627b0fa428e4b26
vmasdani <valianmasdani@gmail.com>
2020-10-04 20:46:49 +0700

    initial population

--- db.go
+++ db.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func DbInit() *gorm.DB {
@@ -61,6 +62,39 @@ func DbInit() *gorm.DB {
 
 	// db.Save(&Partner{
 	// 	Name: "My Partner"})
+	checkAndPopulate(db)
 
 	return db
+}
+
+func checkAndPopulate(db *gorm.DB) {
+	// Populate Service Types
+	fmt.Println("Checking service types...")
+	serviceTypes := []string{
+		"Hotel",
+		"Apartemen",
+		"Kost",
+		"Laundry",
+		"Rental Mobil"}
+
+	for _, serviceTypeName := range serviceTypes {
+		var serviceType ServiceType
+		if res := db.Where("name = ?", serviceTypeName).First(&serviceType); res.Error != nil {
+			fmt.Println(serviceTypeName, "not found! Creating...")
+			db.Save(&ServiceType{Name: serviceTypeName})
+		}
+	}
+
+	// Populate admin login
+	fmt.Println("Checking administrator user...")
+	var user User
+	if res := db.Where("email = ?", "admin@admin.com").First(&user); res.Error != nil {
+		fmt.Println("Admin user not found! Creating...")
+		regularPassword, _ := bcrypt.GenerateFromPassword([]byte("12345678"), bcrypt.DefaultCost)
+		db.Save(&User{
+			Email:    "admin@admin.com",
+			Password: string(regularPassword)})
+	}
+
+	fmt.Println("Population successful!")
 }
